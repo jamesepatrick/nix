f3dc384e73d4d8c0c57405169fc7fe8530a59207
James Patrick <subscriptions@jpatrick.io>
2021-12-13 01:13:26 -0500

    test of flagging system

--- modules/applications/mako.nix
+++ modules/applications/mako.nix
@@ -1,55 +1,70 @@
-{ config, lib, pkgs, ... }: {
+{ options, config, lib, pkgs, ... }:
+let cfg = config.application.mako;
+in with lib; {
+  options = {
+    application.mako = {
+      enable = mkOption {
+        # TODO track based on sway default
+        default = false;
+        type = with types; bool;
+        description = "testing one two three";
+      };
+    };
+  };
 
-  home-manager.users.james = {
-    programs.mako = {
+  config = mkIf cfg.enable {
+
+    home-manager.users.james = {
+      programs.mako = {
+        enable = true;
+        width = 450;
+        padding = "20,20";
+        backgroundColor = "#161720ee";
+        borderColor = "#12151a";
+        textColor = "#d6e5fb";
+        defaultTimeout = 8000;
+        progressColor = "over #d6e5fb";
+        layer = "overlay";
+        groupBy = "summary";
+        extraConfig = ''
+          [hidden]
+          background-color=#bfbfbfff
+          format=+ %h
+
+          [urgency=high]
+          background-color=#1f212eee
+          border-color=#f0185a00
+          text-color=#f0185a
+          ignore-timeout=true
+          default-timeout=0
+
+          [!expiring]
+          background-color=#555555ff
+          text-color=#eeeeeeff
+
+          [grouped]
+          format=<b>%s</b> ✖ %g\n%b
+        '';
+      };
+
+      home.packages = with pkgs; [ libnotify ];
+    };
+
+    systemd.user.services.mako = {
       enable = true;
-      width = 450;
-      padding = "20,20";
-      backgroundColor = "#161720ee";
-      borderColor = "#12151a";
-      textColor = "#d6e5fb";
-      defaultTimeout = 8000;
-      progressColor = "over #d6e5fb";
-      layer = "overlay";
-      groupBy = "summary";
-      extraConfig = ''
-        [hidden]
-        background-color=#bfbfbfff
-        format=+ %h
-
-        [urgency=high]
-        background-color=#1f212eee
-        border-color=#f0185a00
-        text-color=#f0185a
-        ignore-timeout=true
-        default-timeout=0
-
-        [!expiring]
-        background-color=#555555ff
-        text-color=#eeeeeeff
-
-        [grouped]
-        format=<b>%s</b> ✖ %g\n%b
-      '';
+      description = "Mako - Notificaitons for Wayland";
+      documentation = [ "man:mako(5)" ];
+      partOf = [ "sway-session.target" ];
+      bindsTo = [ "sway-session.target" ];
+      environment.PATH = lib.mkForce null;
+      serviceConfig = {
+        ExecStart = "${pkgs.mako}/bin/mako";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+        Type = "simple";
+      };
     };
 
-    home.packages = with pkgs; [ libnotify ];
   };
-
-  systemd.user.services.mako = {
-    enable = true;
-    description = "Mako - Notificaitons for Wayland";
-    documentation = [ "man:mako(5)" ];
-    partOf = [ "sway-session.target" ];
-    bindsTo = [ "sway-session.target" ];
-    environment.PATH = lib.mkForce null;
-    serviceConfig = {
-      ExecStart = "${pkgs.mako}/bin/mako";
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
-      Type = "simple";
-    };
-  };
-
 }
