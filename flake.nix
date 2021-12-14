2c64ba85303b8352dc8391ce662088417ac21923
James Patrick <subscriptions@jpatrick.io>
2021-12-13 22:28:47 -0500

    Switched to using flake-utils
    This reducies the number of things that I will need at sometime in the
    theortical future, by doing more work now.

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.3.0";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -18,35 +19,46 @@
       url = "github:NixOS/nixos-hardware/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  # All outputs for the system (configs)
-  outputs = { home-manager, nixpkgs, nur, nixos-hardware, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config = { allowUnfree = true; };
+  outputs = inputs@{ self, utils, nixpkgs, nixos-hardware, nur, home-manager
+    , emacs-overlay, ... }:
+    utils.lib.mkFlake {
+      hosts = {
+        nil.modules = [
+          nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen1
+          ./hosts/nil.nix
+        ];
       };
-      lib = nixpkgs.lib;
-    in {
-      nixosConfigurations = {
-        # Config is based on hostname
-        nil = lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen1
-            ./hosts/nil.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.james = import ./home.nix;
-            }
-            { nixpkgs.overlays = [ nur.overlay ]; }
-          ];
-        };
 
+      # Shared logic below.
+      inherit self inputs;
+      supportedSystems = [ "x86_64-linux" ];
+      channels.nixpkgs = {
+        input = nixpkgs;
+        overlaysBuilder = channels: [ ];
       };
+      channelsConfig = { allowUnfree = true; };
+      sharedOverlays = [ nur.overlay emacs-overlay.overlay ];
+      hostDefaults = {
+        modules = [
+          nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen1
+          ./hosts/nil.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.james = import ./home.nix;
+          }
+          { nixpkgs.overlays = [ nur.overlay ]; }
+        ];
+        system = "x86_64-linux";
+      };
+
     };
 }
