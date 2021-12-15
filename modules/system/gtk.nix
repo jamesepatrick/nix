326be5ceee5bdcd6e052db4abad08183b573d069
James Patrick <subscriptions@jpatrick.io>
2021-12-14 23:58:51 -0500

    This is now controlled by the graphical flag.

--- modules/system/gtk.nix
+++ modules/system/gtk.nix
@@ -1,18 +1,26 @@
-{ config, pkgs, ... }: {
-  home-manager.users.james = {
-    home.packages = with pkgs; [ dracula-theme ];
-    home.sessionVariables = { GTK_THEME = "Dracula"; };
+{ config, lib, pkgs, ... }:
+let graphical = config.graphical;
+in with lib; {
+  config = mkIf graphical.enable {
+    home-manager.users.james = {
+      home.packages = with pkgs; [
+        dracula-theme
+        moka-icon-theme
+        numix-cursor-theme
+      ];
+      home.sessionVariables = { GTK_THEME = "Dracula"; };
 
-    xdg.configFile."gtk-3.0/settings.ini".text = ''
-      [Settings]
-      gtk-theme-name=Dracula
-      #gtk-icon-theme-name=
-      #gtk-cursor-theme-name=
-      gtk-fallback-icon-theme=gnome
-      gtk-application-prefer-dark-theme=true
-      gtk-xft-hinting=1
-      gtk-xft-hintstyle=hintfull
-      gtk-xft-rgba=none
-    '';
+      xdg.configFile."gtk-3.0/settings.ini".text = ''
+        [Settings]
+        gtk-theme-name=Dracula
+        gtk-icon-theme-name=Moka
+        gtk-cursor-theme-name=Numix-Cursor
+        gtk-fallback-icon-theme=gnome
+        gtk-application-prefer-dark-theme=true
+        gtk-xft-hinting=1
+        gtk-xft-hintstyle=hintfull
+        gtk-xft-rgba=none
+      '';
+    };
   };
 }
