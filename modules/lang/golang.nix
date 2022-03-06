bc36e0c8f174dbb13067bbc25ecfd27e568bdafe
James Patrick <subscriptions@jpatrick.io>
2022-03-06 14:47:09 -0500

    Added a couple missing dev dependencies

--- modules/lang/golang.nix
+++ modules/lang/golang.nix
@@ -18,20 +18,16 @@ in with lib; {
         enable = true;
         goPath = ".local/go";
         goBin = ".local/bin.go";
-        # packages = {
-        #   "golang.org/x/tools/cmd/godoc";
-        #   "golang.org/x/tools/cmd/goimports";
-        #   "golang.org/x/tools/cmd/gorename";
-        #   "golang.org/x/tools/cmd/guru";
-        # };
       };
 
       home.packages = with pkgs; [
         air
+        gcc
         gocode
         golangci-lint
         gomodifytags
         gomodifytags
+        gopls
         gore
         gotests
       ];
