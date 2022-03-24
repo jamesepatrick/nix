eb246e67b203edf1dbeaa8ad38c394bc458e71e0
James Patrick <subscriptions@jpatrick.io>
2022-03-24 18:04:31 -0400

    Added power

---
+++ modules/system/power.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+let cfg = config.this.system.power;
+in with lib; {
+  options.this.system.power.enable = mkEnableOption "Power Management";
+
+  config = mkIf cfg.enable {
+    # fan control modules
+    boot.extraModprobeConfig = ''
+      options thinkpad_acpi fan_control=1 experimental=1
+    '';
+
+    # battery optimizations
+    powerManagement.powertop.enable = true;
+    services.upower.enable = true;
+
+    environment.systemPackages = with pkgs; [ powertop ];
+  };
+}
