4ac76df9a30631b5625941c0caf9b0d93770a03c
James Patrick <subscriptions@jpatrick.io>
2022-07-10 00:35:11 -0400

    Switch to unstable & version bump

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.3.1";
 
     home-manager = {
