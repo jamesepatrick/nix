95e62233400d9366ac269fce3c08e13cdf0249b7
James Patrick <subscriptions@jpatrick.io>
2022-08-14 20:32:59 -0400

    Adding docker support

---
+++ modules/system/docker.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, user, ... }:
+let
+  this = config.system.docker;
+  graphical = config.my.graphical;
+in
+with lib; {
+  options = {
+    system.docker.enable = mkOption {
+      default = true;
+      type = with types; bool;
+    };
+  };
+
+  config = mkIf this.enable {
+    virtualisation.docker = {
+      enable = true;
+      autoPrune.enable = true;
+    };
+
+    users.users."${user.name}".extraGroups = [ "docker" ];
+
+    # This is the old version of docker-compose. run "docker compose" for the v2 implementation.
+    environment.systemPackages = with pkgs; [ docker-compose ];
+  };
+}
