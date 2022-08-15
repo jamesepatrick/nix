fd9c14beed7b7a7f5177933cf4468452edc3ea5c
James Patrick <subscriptions@jpatrick.io>
2022-08-14 21:49:56 -0400

    Refactored gnome to split off normal and extras

--- modules/applications/gnome-common.nix
+++ modules/applications/gnome-common.nix
@@ -1,39 +1,54 @@
 { config, lib, pkgs, user, ... }:
 let
+  gnomeExtras = config.my.application.gnome.extras;
   graphical = config.my.graphical;
-  power = config.my.system.power;
   keyring = config.my.system.keyring;
+  power = config.my.system.power;
 in
 with lib; {
+  options = {
+    my.application.gnome.extras = {
+      enable = mkOption {
+        default = graphical.enable;
+        type = with types; bool;
+      };
+
+      pkgs = mkOption {
+        default = with pkgs.gnome; [
+          cheese
+          file-roller
+          gnome-books
+          gnome-boxes
+          gnome-music
+          iagno
+          pomodoro
+        ];
+        type = with types; listOf package;
+      };
+    };
+  };
   config = mkIf graphical.enable {
     services.gvfs.enable = true;
 
     home-manager.users."${user.name}" = {
       home.packages = with pkgs.gnome;
         [
-          cheese
-          file-roller
           gnome-bluetooth
-          gnome-books
-          gnome-boxes
           gnome-calendar
           gnome-characters
           gnome-color-manager
           gnome-common
           gnome-contacts
-          gnome-control-center
           gnome-dictionary
           gnome-font-viewer
-          gnome-keyring
           gnome-maps
-          gnome-music
-          iagno
-          libgnome-keyring
           nautilus
-          pomodoro
+          nautilus-python
           sushi
-        ] ++ optionals (power.enable) [ gnome-power-manager ]
-        ++ optionals (keyring.enable) [ seahorse ];
+        ]
+        ++ optionals (gnomeExtras.enable) gnomeExtras.pkgs
+        ++ optionals (power.enable) [ gnome-power-manager ]
+        ++ optionals (keyring.enable) [ gnome-keyring libgnome-keyring seahorse ];
     };
   };
 }
