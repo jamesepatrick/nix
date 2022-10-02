bb1894df792637d979aa36d32d2512abaff8fbf8
James Patrick <subscriptions@jpatrick.io>
2022-10-01 22:35:08 -0400

    Adding todoist.

---
+++ modules/applications/todoist.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, user, ... }:
+let
+  this = config.my.application.todoist;
+  graphical = config.my.graphical;
+in
+with lib; {
+  options = {
+    my.application.todoist.enable = mkOption {
+      default = graphical.enable;
+      type = with types; bool;
+    };
+  };
+
+  config = mkIf this.enable {
+    home-manager.users."${user.name}".home.packages = with pkgs; [
+      elementary-planner
+      todoist
+    ];
+  };
+}
