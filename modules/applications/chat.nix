06d50d6e5c895cae03415d8068ecf60096b7bda0
James Patrick <subscriptions@jpatrick.io>
2022-10-06 23:20:20 -0400

    Added basic chat apps

---
+++ modules/applications/chat.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, user, ... }:
+let
+  this = config.my.application.chat;
+  graphical = config.my.graphical;
+in
+with lib; {
+  options = {
+    my.application.chat.enable = mkOption {
+      default = graphical.enable;
+      type = with types; bool;
+    };
+  };
+
+  config = mkIf this.enable {
+    environment.systemPackages = with pkgs;
+      [
+        discord
+        element-desktop
+      ];
+  };
+}
