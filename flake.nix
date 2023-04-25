439c1fff5e376dcb88d4b236edcd6c67842d5e06
James Patrick <subscriptions@jpatrick.io>
2023-04-24 22:57:53 -0400

    Removed unused import

--- flake.nix
+++ flake.nix
@@ -13,12 +13,11 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
-    sops-nix.url = "github:Mic92/sops-nix";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.3.1";
   };
 
   outputs = inputs@{ self, emacs-overlay, home-manager, nixos-hardware, nixpkgs
-    , nur, utils, sops-nix, ... }:
+    , nur, utils, ... }:
     let
       inherit (utils.lib) mkFlake;
       inherit (self.lib.my) mapModules mapModulesRec';
@@ -51,8 +50,7 @@
             description = "James Patrick";
           };
         };
-        modules = mapModulesRec' ./modules import
-          ++ [ sops-nix.nixosModules.sops ];
+        modules = mapModulesRec' ./modules import;
         system = "x86_64-linux";
       };
     };
