3f007d037ca5d7d33dddb588ca49333fb6b0a783
James Patrick <subscriptions@jpatrick.io>
2023-05-15 21:32:44 -0400

    Added pcscd required for gpg tooling

--- modules/system/yubikey.nix
+++ modules/system/yubikey.nix
@@ -2,15 +2,16 @@
 let
   this = config.my.system.yubikey;
   graphical = config.my.graphical;
-in
-with lib; {
+in with lib; {
   options.my.system.yubikey.enable = mkEnableOption "Yubikey";
 
   config = mkIf this.enable (mkMerge [
     {
-      services.udev.packages = with pkgs; [ yubikey-personalization ];
+      services = {
+        udev.packages = with pkgs; [ yubikey-personalization ];
+        pcscd.enable = true;
+      };
       hardware.gpgSmartcards.enable = true;
-
       security.pam = {
         yubico = {
           enable = true;
@@ -20,21 +21,21 @@ with lib; {
       };
 
       environment.systemPackages = with pkgs; [ pinentry-curses yubico-pam ];
-      home-manager.users."${user.name}".home.packages = with pkgs;
-        [ yubikey-manager yubikey-personalization ];
+      home-manager.users."${user.name}".home.packages = with pkgs; [
+        yubikey-manager
+        yubikey-personalization
+      ];
     }
     (mkIf graphical.enable {
 
-      environment.systemPackages = with pkgs;
-        [ pinentry-gnome ];
+      environment.systemPackages = with pkgs; [ pinentry-gnome ];
 
       home-manager.users."${user.name}".home = {
-        packages = with pkgs;
-          [
-            yubikey-touch-detector
-            yubikey-manager-qt
-            yubikey-personalization-gui
-          ];
+        packages = with pkgs; [
+          yubikey-touch-detector
+          yubikey-manager-qt
+          yubikey-personalization-gui
+        ];
       };
 
       systemd.user.services.touchthebutton = {
@@ -43,7 +44,8 @@ with lib; {
         wantedBy = [ "graphical-session.target" ];
         partOf = [ "graphical-session.target" ];
         serviceConfig = {
-          ExecStart = "${pkgs.yubikey-touch-detector}/bin/yubikey-touch-detector -v -libnotify";
+          ExecStart =
+            "${pkgs.yubikey-touch-detector}/bin/yubikey-touch-detector -v -libnotify";
           RestartSec = 5;
           Restart = "always";
         };
