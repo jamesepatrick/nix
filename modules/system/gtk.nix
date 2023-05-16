bd8303d3c47be80a4f205496e86397135b206ddf
James Patrick <subscriptions@jpatrick.io>
2023-05-15 21:33:32 -0400

    Use gtk extra config options instead of writing setting.ini

--- modules/system/gtk.nix
+++ modules/system/gtk.nix
@@ -2,31 +2,27 @@
 let
   graphical = config.my.graphical;
   this = config.my.system.gtk;
-in
-with lib; {
-  options.my.system.gtk = {
-    theme = mkOption {
-      default = "Dracula";
-      type = with types; str;
-    };
-  };
+in with lib; {
   config = mkIf graphical.enable {
     home-manager.users."${user.name}" = {
-      gtk.enable = true;
-      home.packages = with pkgs; [ dracula-theme kora-icon-theme ];
-      home.sessionVariables = { GTK_THEME = "${this.theme}"; };
-      systemd.user.sessionVariables = { GTK_THEME = "${this.theme}"; };
-
-      xdg.configFile."gtk-3.0/settings.ini".text = ''
-        [Settings]
-        gtk-theme-name=Dracula
-        gtk-icon-theme-name=kora
-        gtk-fallback-icon-theme=gnome
-        gtk-application-prefer-dark-theme=true
-        gtk-xft-hinting=1
-        gtk-xft-hintstyle=hintfull
-        gtk-xft-rgba=none
-      '';
+      gtk = {
+        enable = true;
+        gtk3.extraConfig = {
+          gtk-fallback-icon-theme = "gnome";
+          gtk-application-prefer-dark-theme = true;
+          gtk-xft-hinting = 1;
+          gtk-xft-hintstyle = "hintfull";
+          gtk-xft-rgba = "none";
+        };
+        iconTheme = {
+          package = pkgs.kora-icon-theme;
+          name = "kora";
+        };
+        theme = {
+          package = pkgs.dracula-theme;
+          name = "Dracula";
+        };
+      };
     };
   };
 }
