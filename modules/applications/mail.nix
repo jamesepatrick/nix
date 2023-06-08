e925ef12c592154a8873f10d5a867933858df2db
James Patrick <subscriptions@jpatrick.io>
2023-06-07 20:00:36 -0400

    Fixed mail to be actually useful.

--- modules/applications/mail.nix
+++ modules/applications/mail.nix
@@ -2,32 +2,75 @@
 let
   this = config.my.application.mail;
   graphical = config.my.graphical;
-in
-with lib; {
+  secrets = config.my.secrets;
+in with lib; {
   options = {
-    my.application.mail.enable = mkOption {
-      default = graphical.enable;
-      type = with types; bool;
+    my.application.mail = {
+      maildir = mkOption { default = "/home/${user.name}/.cache/mail"; };
+      enable = mkOption {
+        default = graphical.enable;
+        type = with types; bool;
+      };
     };
   };
 
   config = mkIf this.enable {
-
+    assertions = [{
+      assertion = config.my.system.secrets.enable;
+      message = "This module requires the use of secrets";
+    }];
     home-manager.users."${user.name}" = {
-      home.packages = with pkgs; [ protonmail-bridge mu isync ];
+      home.packages = with pkgs; [
+        isync
+        key
+        libsecret # used to access gnome keychain
+        mu
+        protonmail-bridge
+      ];
 
-      programs.mbsync = {
-        enable = true;
-      };
+      programs.mbsync.enable = true;
       programs.mu.enable = true;
 
-      # accounts.email.accounts."ProtonMail" = {
-      #   address = "public@jpatrick.io";
-      #   mu.enable = true;
-      # };
+      accounts.email = {
+        maildirBasePath = "${this.maildir}";
+        accounts."ProtonMail" = {
+          address = secrets.email.primary;
+          aliases = secrets.email.aliases;
+          userName = secrets.email.primary;
+          passwordCommand =
+            "${pkgs.libsecret}/bin/secret-tool lookup proto IMAP host 127.0.0.1";
+          primary = true;
+          realName = "${name}";
+          mbsync = {
+            enable = true;
+            create = "both";
+            expunge = "both";
+            patterns = [ "*" ];
+          };
+          mu.enable = true;
+          imap = {
+            host = "127.0.0.1";
+            port = 1143;
+            tls = {
+              enable = true;
+              useStartTls = true;
+              certificatesFile =
+                "/home/${user.name}/.config/protonmail/bridge/cert.pem";
+            };
+          };
+        };
+      };
+
+      services = {
+        mbsync = {
+          enable = true;
+          frequency = "*:0/15";
+          preExec = "${pkgs.isync}/bin/mbsync -Ha";
+          postExec = "${pkgs.mu}/bin/mu index";
+        };
+      };
+
     };
-
-
     systemd.user.services = {
       protonbridge = {
         enable = true;
