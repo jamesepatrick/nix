1450f057e7d904b50f51e99e6332b790c57ef958
James Patrick <public@jpatrick.io>
2023-11-08 20:47:51 -0500

    Removed unused battery related package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,4 @@
 self: super: {
-  battery-notify = super.callPackage ./battery-notify.nix { inherit super; };
   brightness-sh = super.callPackage ./brightness.nix { inherit super; };
   plymouth-themes = super.callPackage ./plymouth-themes.nix { inherit super; };
   schubsigo = super.callPackage ./schubsigo { inherit super; };
