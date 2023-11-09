4937b115903ba3381af913954a03883e304046fb
James Patrick <public@jpatrick.io>
2023-11-08 20:49:21 -0500

    Relocated schubsigo

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,6 @@
 self: super: {
   brightness-sh = super.callPackage ./brightness.nix { inherit super; };
   plymouth-themes = super.callPackage ./plymouth-themes.nix { inherit super; };
-  schubsigo = super.callPackage ./schubsigo { inherit super; };
+  schubsigo = super.callPackage ./schubsigo.nix { inherit super; };
   volume-sh = super.callPackage ./volume.nix { inherit super; };
 }
